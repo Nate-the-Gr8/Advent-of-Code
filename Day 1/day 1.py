e7e98366077c8851205203582e64d0f75e4fe97a
Nathaniel <nathaniel.girard.1@ulaval.ca>
2021-12-16 11:57:31 -0500

    day 1 done!

---
+++ Day 1/day 1.py
@@ -0,0 +1,26 @@
+# Part 1
+"""
+with open("input.txt", "r", encoding="utf-8") as file:
+    increment_count = -1
+    previous_line = 0
+    for line in file.readlines():
+        line = int(line)
+        if line > previous_line:
+            increment_count += 1
+        previous_line = line
+print(increment_count)
+"""
+
+
+with open("C:\\Users\\natha\\Desktop\\Python\\Advent-of-Code\\Day 1\\input.txt", "r", encoding="utf-8") as file:
+    data = [int(line) for line in file.read().splitlines()]
+    increment_count, last = -1, 0
+    for i, value in enumerate(data):
+        try:
+            current = value + data[i+1] + data[i+2]
+            if current > last:
+                increment_count += 1
+            last = current
+        except IndexError:
+            break
+print(increment_count)
