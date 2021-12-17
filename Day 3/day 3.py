4acd967629dda48c6f5496b0b6bbc0bdcdacb9d1
Nathaniel <nathaniel.girard.1@ulaval.ca>
2021-12-17 02:42:41 -0500

    day 3 almost done

--- Day 3/day 3.py
+++ Day 3/day 3.py
@@ -1,3 +1,6 @@
+from functools import partial
+
+
 # Part 1
 """
 with open("input.txt", "r", encoding="utf-8") as file:
@@ -5,7 +8,7 @@ with open("input.txt", "r", encoding="utf-8") as file:
     lines = file.readlines()
     for column in range(len(lines[0])-1):
         mostcommon = sum([int(line[column]) for line in lines])
-        if mostcommon > 500:
+        if mostcommon > len(lines)/2:
             gamma += "1"
             epsilon += "0"
         else:
@@ -18,11 +21,19 @@ print(int(gamma, 2)*int(epsilon, 2))
 # """
 with open("input.txt", "r", encoding="utf-8") as file:
     lines = file.readlines()
-    o2 = max(set(lines), key=lines.count)
-    co2 = min(set(lines), key=lines.count)
-print(int(o2, 2)*int(co2, 2))
-
-
-    
-# print(int(gamma, 2)*int(epsilon, 2))
+    o2key, co2key = "", ""
+    file = " " + " ".join(lines)
+    for column in range(len(lines[0])-1):
+        mostcommon = sum([int(line[column]) for line in lines])
+        if mostcommon >= len(lines)/2:
+            o2key += "1"
+            co2key += "0"
+        else:
+            o2key += "0"
+            co2key += "1"
+        if file.count(" " + o2key) == 1:
+            o2filter = lines[file.find(" " + o2key)//(len(lines[0])+1)]
+        if file.count(" " + co2key) == 1:
+            co2filter = lines[file.find(" " + co2key)//(len(lines[0])+1)]
+print(int(o2filter, 2)*int(co2filter, 2))
 # """
